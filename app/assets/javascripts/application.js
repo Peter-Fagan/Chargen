b4ac2c92c63981d7f7fc58a0d7ebd2eccb42be95
Peter-Fagan <peter_fagan@hotmail.com>
2016-11-17 18:59:01 +1100

    format of seeds, added tooltips for race, prof, bg

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,34 @@
 //= require turbolinks
 //= require materialize-sprockets
 //= require_tree .
+
+// Regex.Match(text, ([^<]*));
+
+$(document).ready(function() {
+
+    $("#raceInfo label").hover(function() {
+        var race1 = $(this).attr("for");
+        var race2 = race1.replace("character_race_id_", "#race");
+        console.log(race2);
+        $(race2).show();
+    }, function() {
+        $(".racePopup").hide();
+    });
+
+    $("#profInfo label").hover(function() {
+        var prof1 = $(this).attr("for");
+        var prof2 = prof1.replace("character_profession_id_", "#prof");
+        console.log(prof2);
+        $(prof2).show();
+    }, function() {
+        $(".profPopup").hide();
+    });
+
+    $("#bgInfo label").hover(function() {
+        var bg1 = $(this).attr("for");
+        var bg2 = bg1.replace("character_background_id_", "#bg");
+        $(bg2).show();
+    }, function() {
+        $(".bgPopup").hide();
+    });
+});
